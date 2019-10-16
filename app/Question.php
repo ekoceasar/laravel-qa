b4bbfd1c36983bd22e3bc6f5f4be03a2fffea853
ekoceasar <ekoceasar@gmail.com>
2019-10-16 23:40:22 +0100

    Prepopulate users and questions tables using model factories and seed

--- app/Question.php
+++ app/Question.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    protected $fillable = ['title', 'body'];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value){
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);// from laravel/helpers
+    }
 }
---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(App\Question::class, function (Faker $faker) {
+    return [
+        'title' => rtrim($faker->sentence(rand(5,10)), "."),//remove the dot at the end of the generated sentence
+        'body' => $faker->paragraphs(rand(3,7), true), //true parameter generates a string instead of an array
+        'views' => rand(0,10),
+        'answers' => rand(0,10),
+        'votes' => rand(0,10)
+    ];
+});
